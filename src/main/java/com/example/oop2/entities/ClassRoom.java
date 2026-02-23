edee3377d34f78c3655eb041a94f04930ff8fd8d
AshhCh <150962623+AshhCh@users.noreply.github.com>
2026-02-23 00:17:06 -0300

    Updated ClassRooom.java

--- src/main/java/com/example/oop2/entities/ClassRoom.java
+++ src/main/java/com/example/oop2/entities/ClassRoom.java
@@ -1,4 +1,38 @@
 package com.example.oop2.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ClassRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String classroomNumber;
+
+    private String type;
+
+    private int capacity;
+
+    private boolean isAvailable;
+
 }
