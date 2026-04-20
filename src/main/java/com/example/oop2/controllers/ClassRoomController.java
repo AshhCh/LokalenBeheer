565e00988969485927b017622cd5ea7263b96b3f
Ashhch <ashleyhoiting.cheung@gmail.com>
2026-04-20 10:50:35 -0300

    classroom getId and delete added and optimized

--- src/main/java/com/example/oop2/controllers/ClassRoomController.java
+++ src/main/java/com/example/oop2/controllers/ClassRoomController.java
@@ -3,6 +3,7 @@ package com.example.oop2.controllers;
 import com.example.oop2.dtos.CreateClassRoomRequest;
 import com.example.oop2.entities.ClassRoom;
 import com.example.oop2.services.IClassRoomService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +15,30 @@ public class ClassRoomController {
     // Note: We use the Interface here, not the Implementation!
     private final IClassRoomService classroomService;
 
+    //CONSTRUCTOR INJECTION
     public ClassRoomController(IClassRoomService classroomService) {
         this.classroomService = classroomService;
     }
+
+    //CREATE CLASSROOM(POST)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClassRoom create(@RequestBody CreateClassRoomRequest request) {
+        return classroomService.create(request);
+    }
+
     // GET ALL CLASSROOMS
     @GetMapping
     public List<ClassRoom> getAll() {
         return classroomService.getAll();
     }
 
-    @PostMapping
-    public ClassRoom create(@RequestBody CreateClassRoomRequest request) {
-        return classroomService.create(request);
-    }
-
     //GET CLASSROOM BY ID
+    @GetMapping("/{id}")
+    public ClassRoom getById(@PathVariable Long id) { return classroomService.getById(id); }
 
-
-    //DELETE STUDENT BY ID
+    //DELETE CLASSROOM BY ID
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){ classroomService.delete(id); }
 }
