56c36a92e823e1d767dec3c9a671be3924a53cd3
AshhCh <150962623+AshhCh@users.noreply.github.com>
2026-05-02 13:08:05 -0300

    added input validation and gloabl exception handler

---
+++ src/main/java/com/example/oop2/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.oop2.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice //class handles exceptions from all of the controllers
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgument(IllegalArgumentException ex){
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", 400);
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
